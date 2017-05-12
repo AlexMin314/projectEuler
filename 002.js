d07f82b2892efd8b07f614b56d9841bc815a7894
Alex Min <m98stone@gmail.com>
2017-05-12 14:46:52 +0800

    Solution 002

--- 002.js
+++ 002.js
@@ -1,12 +1,19 @@
 // Version 1.0
 
 function fibonacci(num) {
+  // making fibo array.
   let arr = [1, 2, ];
   for (let i = 0; arr[i] < num; i += 1) {
-      arr.push(arr[i]+arr[i + 1]);
+    arr.push(arr[i] + arr[i + 1]);
   }
-  arr = arr.filter(function(e) {return e % 2 === 0;});
-  console.log(arr.reduce(function (a,b) {return a + b;}));
+  // filter even element and sum.
+  arr = arr.filter(function(e) {
+    return e % 2 === 0;
+  }).reduce(function(a, b) {
+    return a + b;
+  });
+  return arr;
 }
 
-fibonacci(4000000);
+console.log(fibonacci(4000000));
+// 4613732 [Finished in 0.074s]
