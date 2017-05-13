5b5c8582e0d7f090f5891201ca1a429e1693a76d
Alex Min <m98stone@gmail.com>
2017-05-13 11:08:52 +0800

    Solution 014

--- 014.js
+++ 014.js
@@ -1,3 +1,29 @@
 // Version 1.0
+function collatzCounter(n) {
+  let count = 1;
+  while (n > 1) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      n = n * 3 + 1;
+    }
+    count++;
+  }
+  return count;
+}
 
-// under progress
+function maxChecker(value) {
+  let max = {'count' : 0, 'index' : 0}
+  let i, count;
+  for(i = 1; i <= value; i++) {
+    count = collatzCounter(i)
+    if (count > max.count) {
+        max.count = count;
+        max.index = i;
+    }
+  }
+  return max;
+}
+
+console.log(maxChecker(1000000));
+// { count: 525, index: 837799 } [Finished in 2.103s]
